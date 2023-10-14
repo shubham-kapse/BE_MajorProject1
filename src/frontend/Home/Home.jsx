c14c2bc441106ed48579c9587bfabdd884d97404
Yash <kudveyash@gmail.com>
2023-10-14 12:20:24 +0530

    UI changes

--- src/frontend/Home/Home.jsx
+++ src/frontend/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Home.css'
 import homeimg1 from '../../images/homeimg1.png'
+import aboutimg from '../../images/image-about-us.png'
+import CountUp from 'react-countup'
 
 const Home = () => {
     return (
@@ -21,7 +23,37 @@ const Home = () => {
 
                 </div>
             </div>
-        </div>
+
+            <div className='counter'>
+                <h1>
+                    Counter
+                    {/* <CountUp start={0} end={6} duration={5} delay={0} /> */}
+                </h1>
+
+            </div>
+
+            <div className='aboutus'>
+                <div className='lc'>
+                    <img src={aboutimg} alt="About us image" />
+                </div>
+
+                <div className='rc'>
+
+                    <h1>About Us</h1>
+                    <span>Welcome to Mental Health Tracker, where we believe that taking care of your mental well-being is a journey worth tracking. We understand that mental health is a vital component of overall wellness, and we're here to support you every step of the way. <br />
+
+                        Our mission is to empower individuals to prioritize their mental health, reduce stigma, and promote self-awareness. We provide a safe and compassionate space for you to monitor and improve your mental health, because we believe that everyone deserves access to the resources and tools they need to thrive.
+                    </span>
+                    <br />
+                    <span>
+                        We invite you to explore our website, use our tracking tools, access valuable resources, and become a part of our supportive community. Together, we can break the silence surrounding mental health, foster self-care, and empower one another to lead happier, healthier lives.
+
+                        Thank you for choosing Mental Health Tracker as your partner on this journey. Your well-being matters, and we're here to help you prioritize it.
+                    </span>
+
+                </div>
+            </div>
+        </div >
     )
 }
 
--- src/frontend/Navbar/Navibar.jsx
+++ src/frontend/Navbar/Navibar.jsx
@@ -4,49 +4,49 @@ import './Navibar.css'
 const Navibar = () => {
     return (
         <div className='Navibar'>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light" id='navHeader'>
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Logo Image</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navHeader'>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Logo Image</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarScroll">
-                        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
-                            <li class="nav-item">
-                                <a class="nav-link active" id='lihover' aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarScroll">
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
+                            <li className="nav-item">
+                                <a className="nav-link active" id='lihover' aria-current="page" href="/">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id='lihover' href="/">About Us</a>
+                            <li className="nav-item">
+                                <a className="nav-link" id='lihover' href="/">About Us</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="/" id="navbarScrollingDropdown lihover" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" id="navbarScrollingDropdown lihover" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Services
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                                    <li><a class="dropdown-item" href="/">Action</a></li>
-                                    <li><a class="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="/">Something else here</a></li>
+                                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a className="dropdown-item" href="/">Action</a></li>
+                                    <li><a className="dropdown-item" href="/">Another action</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
                                 </ul>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="/" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Pages
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                                    <li><a class="dropdown-item" href="/">Action</a></li>
-                                    <li><a class="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="/">Something else here</a></li>
+                                <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a className="dropdown-item" href="/">Action</a></li>
+                                    <li><a className="dropdown-item" href="/">Another action</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
                                 </ul>
                             </li>
-                            {/* <li class="nav-item">
-                                <a class="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Link</a>
+                            {/* <li className="nav-item">
+                                <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Link</a>
                             </li> */}
                         </ul>
-                        <form class="d-flex">
-                            {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
-                            <button class="btn btn-outline-success CUbtn" type="submit">Contact Us</button>
+                        <form className="d-flex">
+                            {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> */}
+                            <button className="btn btn-outline-success CUbtn" type="submit">Contact Us</button>
                         </form>
                     </div>
                 </div>
